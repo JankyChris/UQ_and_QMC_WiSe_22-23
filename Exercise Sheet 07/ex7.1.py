87144cfc5782a4fb2477120f01fcf8ab419b87c7
JankyChris <chrissi@janky.de>
2022-12-07 11:01:56 +0100

    update

--- Exercise Sheet 07/ex7.1.py
+++ Exercise Sheet 07/ex7.1.py
@@ -4,77 +4,82 @@
 import numpy as np
 import scipy.io
 from scipy import sparse
-from scipy.sparse.linalg import eigsh
+from scipy.sparse.linalg import eigsh, spsolve
+import matplotlib.pyplot as plt
+from joblib import Parallel, delayed
 
-def FEMdata():
+def FEMdata(path="Exercise Sheet 07/week7.mat"):
     """
     Load FEM matrices from "week7.mat" file and convert MATLAB-specifics
     for use with python3.
     """
-    mat = scipy.io.loadmat("week7.mat")
+    mat = scipy.io.loadmat(path)
 
-    grad = mat["grad"]
-    mass = mat["mass"]
-    nodes = mat["nodes"]
-    element = -1+mat['element']                 # Python-style indexing
-    interior = -1+np.array(mat['interior'][0])  # Python-style indexing
-    centers = mat["centers"]
-    ncoord = mat['ncoord'][0,0]                 # turn into a scalar
-    nelem = mat['nelem'][0,0]                   # turn into a scalar
+    grad = mat["grad"]                      # gradient part of stiffness matrix/tensor
+    mass = mat["mass"]                      # mass matrix
+    nodes = mat["nodes"]                    # FE nodes
+    element = -1 + mat['element']           # indices for the elements (Python-style indexing)
+    interior = -1 + mat['interior'][:, 0]   # indices for the interior nodes (Python-style indexing)
+    centers = mat["centers"]                # coordinates of the element centers
+    ncoord = mat['ncoord'][0,0]             # number of FE nodes (to scalar)
+    nelem = mat['nelem'][0,0]               # Number of elements (to scalar)
 
 
     return grad, mass, nodes, element, interior, centers, ncoord, nelem
 
-def a(centers, y):
+def a(centers, y, s=100, decay=2.0):
     """
     Definition of the diffusion coefficient.
     """
+    indices = np.arange(1, s+1).reshape((s, 1))
+    deterministic = indices**(-decay) * np.sin(np.pi * indices * centers[:,0]) * np.sin(np.pi * indices * centers[:,1])
 
-    return k**(-2) * np.sin(np.pi*k*centers[:,0]) * np.sin(np.pi*k*centers[:,1])
-    
-    """
-    kmax = len(y)
-    val = []
+    return 2 + y @ deterministic
 
-    for k in range(kmax):
-        val.append(y[k] * psi(k, x))
-
-    return 2 + np.sum(val)
-    """
 
 def UpdateStiffness(grad, a):
+    """
+    Generates the stiffness matrix from given grad and diffusion coefficient a
+    """
     n = np.sqrt(grad.shape[0]).astype(int)
-    return (grad@sparse.csr_matrix(a.reshape((a.size,1)))).reshape((n,n))
+    vec = grad @ sparse.csr_matrix(a.reshape((a.size, 1)))
 
-def MonteCarlo(y, x, u, n, s=100):
-    us = [u(x, yi) for yi in y]
-    
-    return (1/n * np.sum(us))
+    return sparse.csr_matrix.reshape(vec,(n,n)).tocsr()
+
+
+# TODO: Monte Carlo sampler
+
+def MonteCarlo():
+    ...
+
+def plotPDE(nodes, element, result):
+    fig = plt.figure(figsize=plt.figaspect(1.0))
+    ax = fig.add_subplot(1,1,1,projection="3d")
+    ax.plot_trisurf(nodes[:, 0], nodes[:, 1], result, triangles=element, cmap="viridis")
+    plt.show()
 
 if __name__ == "__main__":
     # use n == 2^maxiter as the "reference solution"
     maxiter = 20;
 
+    # set stochastic dimension
+    s = 100
+
     # load FEM data from MATLAB file
     grad, mass, nodes, element, interior, centers, ncoord, nelem = FEMdata()
 
-    # define source term ( f(x)=x_1 )
-    f = lambda x: x[:, 0]
-    # define loading vector
-    rhs = mass[interior, :] @ f(nodes)
+    # set up the loading term
+    f = lambda x: x[:, 0]               # define source term ( f(x)=x_1 )
+    rhs = mass[interior, :] @ f(nodes)  # define loading vector
 
-    sums = []
-    means = []
-    null = np.zeros(ncoord, 1)
+    ## Part 1: Source problem
+    # Sanity check: Can we solve the PDE for one realization of the uniform random vector y?
+    print('Solve one PDE source problem')
+    mcnode = np.random.uniform(-1/2,1/2,s)
+    A = UpdateStiffness(grad,a(centers, mcnode, s))
+    sol = np.zeros(ncoord)
+    tmp = spsolve(A[np.ix_(interior,interior)], rhs)
+    sol[interior] = tmp
 
-    # range over an increasing number of random samples
-    for i in range(maxiter):
-        print("Iteration: ", i)
-        n = max(1, 2**(i-1))
-        tmp = np.zeros(ncoord, 1)
-        # main loop
-        for k in range(1, n+1):
-            mcnode = np.random.uniform(low=-0.5, high=0.5, size=(n,s))
-            A = UpdateStiffness(grad, a(centers, mcnode))
-            sol = null
-            sol[interior] = np.linalg.solve(A[interior, interior], rhs)
+    # Visualize the results
+    plotPDE(nodes, element, sol)
